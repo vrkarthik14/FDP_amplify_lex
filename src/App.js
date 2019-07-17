11c0a9b902bbffaec020a5fd1a8d7ec45255f5d8
vrkarthik14 <vrkarthik14@gmail.com>
2019-07-17 23:02:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withAuthenticator } from "aws-amplify-react";
+import React, { Component } from "react";
+import Amplify, { Interactions } from "aws-amplify";
+import { ChatBot, AmplifyTheme } from "aws-amplify-react";
+import awsconfig from "./aws-exports";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsconfig);
+
+// Imported default theme can be customized by overloading attributes
+const myTheme = {
+  ...AmplifyTheme,
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: "#ff6600"
+  }
+};
+
+class App extends Component {
+  handleComplete(err, confirmation) {
+    if (err) {
+      alert("Bot conversation failed");
+      return;
+    }
+
+    alert("Success: " + JSON.stringify(confirmation, null, 2));
+    return "Trip booked. Thank you! what would you like to do next?";
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to ChatBot Demo</h1>
+        </header>
+        <ChatBot
+          title="My Bot"
+          theme={myTheme}
+          botName="ScheduleAppointment_env`"
+          welcomeMessage="Welcome, how can I help you today?"
+          onComplete={this.handleComplete.bind(this)}
+          clearOnComplete={true}
+          conversationModeOn={false}
+        />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, true);
